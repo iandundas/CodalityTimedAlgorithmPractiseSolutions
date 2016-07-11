e14e13f51c37de5963f098fa5e3d7d224bd7f259
Ian Dundas <contact@iandundas.co.uk>
2016-07-11 12:25:59 +0200

    gave up on PermMissingElem

---
+++ 3-TimeComplexity/PermMissingElem.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+public func solution(inout A : [Int]) -> Int {
+    
+    // can't be arsed solving this one, knowledge of maths solution I didn't have, but once known is too easy:
+    // "This problem has a mathematical solution, based on the fact that the sum of consecutive integers from 1 to n is equal to n(n+1)/2."
+    // http://stackoverflow.com/questions/29861306/what-is-the-right-way-to-solve-codilitys-permmissingelem-test-java
+    
+    
+    return 0
+}
+
+var input = [2,3,1,5]
+solution(&input)
--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Find longest sequence of zeros in binary representation of an integer.
 1. CyclicRotation ([score: 100%](https://codility.com/demo/results/trainingGBGCAQ-UTA/))
 Rotate an array to the right by a given number of steps.
 
-2. OddOccurrencesInArray (*[score](https://codility.com/demo/results/trainingZA7327-UGD/): 100%*), 
+2. OddOccurrencesInArray ([score](https://codility.com/demo/results/trainingZA7327-UGD/): 100%), 
 Find value that occurs in odd number of elements.
 
 ### Time Complexity
